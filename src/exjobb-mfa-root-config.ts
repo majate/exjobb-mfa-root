b59d3e4a65f17c21403ee7506855469f5ba1abf9
majate <majate@kth.se>
2022-06-11 00:47:41 +0200

    Init prettier

--- src/exjobb-mfa-root-config.ts
+++ src/exjobb-mfa-root-config.ts
@@ -1,20 +1,21 @@
-import { registerApplication, start } from "single-spa";
+import { registerApplication, start } from 'single-spa'
 
 registerApplication({
-  name: "@single-spa/welcome",
+  name: '@single-spa/welcome',
   app: () =>
     System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+      'https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js'
     ),
   activeWhen: location => location.pathname === '/',
-});
+})
 
 registerApplication({
-  name: "@exjobb-mfa/form",
-  app: () => System.import("@exjobb-mfa/form"),
-  activeWhen: ["/form"]
-});
+  name: '@exjobb-mfa/form',
+  app: () => System.import('@exjobb-mfa/form'),
+  activeWhen: ['/form'],
+})
 
 start({
   urlRerouteOnly: true,
-});
+})
+
