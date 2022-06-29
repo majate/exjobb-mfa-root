d0bad6d2633f10fbdc0e8752f1105f9e4bb5d678
majate <majate@kth.se>
2022-06-29 18:15:04 +0200

    Fixed divs for micro frontends

--- src/exjobb-mfa-root-config.ts
+++ src/exjobb-mfa-root-config.ts
@@ -13,6 +13,7 @@ registerApplication({
     title: 'EXJOBB',
     titlePath: '/',
     menuItems,
+    domElement: document.getElementById('header'),
   },
 })
 
@@ -20,18 +21,21 @@ registerApplication({
   name: '@exjobb-mfa/home',
   app: () => System.import('@exjobb-mfa/home'),
   activeWhen: location => location.pathname === '/',
+  customProps: { domElement: document.getElementById('content') },
 })
 
 registerApplication({
   name: '@exjobb-mfa/form',
   app: () => System.import('@exjobb-mfa/form'),
   activeWhen: ['/form'],
+  customProps: { domElement: document.getElementById('content') },
 })
 
 registerApplication({
   name: '@exjobb-mfa/feed',
   app: () => System.import('@exjobb-mfa/feed'),
   activeWhen: ['/feed'],
+  customProps: { domElement: document.getElementById('content') },
 })
 
 start({
